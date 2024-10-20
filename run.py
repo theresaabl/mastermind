f61e6a204ba733e82601b7e6ddf1562e83f85028
Theresa Abl <theresaabl@gmx.at>
2024-10-20 08:04:11 +0000

    Create Game class

--- run.py
+++ run.py
@@ -1,4 +1,16 @@
 #mock terminal of 80 characters wide and 24 rows high
 
-print("\nMASTERMIND\n")
-print("Welcome to a game of Mastermind.\n")
+class Game:
+    def __init__(self, colors = [f"{i}" for i in range(1, 7)], code_length = 4, max_attempts = 12):
+        self.colors = colors
+        self.code_length = code_length
+        self.max_attempts = max_attempts
+    
+    def run_game(self):
+        print("\nMASTERMIND\n")
+        print("Welcome to a game of Mastermind.\n")
+
+
+# Run game
+game = Game()
+game.run_game()
